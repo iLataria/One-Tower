68bd1b24ec844b5d2422a2070dda19c5e9a2a00a
Glebov Vlad <glebowladic@gmail.com>
2024-10-30 13:48:02 +0300

    Added simple enemySpawner

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] enemyPrefab;
+
+    [SerializeField]
+    private Transform[] spawnPoints;
+
+    [SerializeField]
+    private float spawnDelay;
+
+    public float startSpawnInterval;
+
+    [SerializeField]
+    private int enemyCount;
+    [SerializeField]
+    private int nowEnemies;
+
+    private int randEnemy;
+    private int randomPoint;
+
+   
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnDelay= startSpawnInterval;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (spawnDelay <= 0 && nowEnemies < enemyCount)
+        {
+            randEnemy = Random.Range(0, enemyPrefab.Length);
+            randomPoint = Random.Range(0, spawnPoints.Length);
+
+            Instantiate(enemyPrefab[randEnemy], spawnPoints[randomPoint].transform.position,
+                Quaternion.identity);
+
+            spawnDelay = startSpawnInterval;
+            nowEnemies++;
+        }
+        else {
+        
+            spawnDelay -= Time.deltaTime;
+        }
+    }
+}
