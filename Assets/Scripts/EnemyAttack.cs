9a46ded56e69de4bc10ddbce3314f7238c25bd3e
Glebov Vlad <glebowladic@gmail.com>
2024-11-06 15:42:33 +0300

    Fixed enemyAttack
    now Tower get damage every N seconds while enemy is in Trigger zone

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -1,4 +1,5 @@
 using AloneTower.SpawnSystem;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,15 +10,27 @@ namespace AloneTower
         [SerializeField]
         private float enemyDamage= 1f;
 
+        [SerializeField]
+        private float damageRate=3f;
+
         // made reference only with public;
         public Slider healthSlider;
 
-        private void OnTriggerStay()
+        private void OnTriggerEnter()
         {
-           healthSlider.value-= enemyDamage*Time.deltaTime;
-          
+          StartCoroutine(DamageRating());
+          Debug.Log(damageRate);
         }
 
+        private IEnumerator DamageRating() 
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(damageRate);
+                healthSlider.value -= enemyDamage;
+            }
+            
+        }
         
     }
 
