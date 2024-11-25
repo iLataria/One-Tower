b30a1151012f665362149b40fe67e9d42e421d05
Glebov Vlad <glebowladic@gmail.com>
2024-11-25 09:40:25 +0300

    Added smooth Alpha change

--- Assets/Scripts/UIComboSystem.cs
+++ Assets/Scripts/UIComboSystem.cs
@@ -7,7 +7,9 @@ public class UIComboSystem : MonoBehaviour
     private float sliderCurrentWidth;
     private float sliderMinWidth = 30f;
     private float sliderMaxWidth = 250f;
-
+    private float alphaChangeSpeed = 0.3f;
+    private float leftLimit = 110f;
+    private float rightLimit = 170f;
     [SerializeField]
     private RectTransform sliderTransform;
     [SerializeField]
@@ -20,34 +22,59 @@ public class UIComboSystem : MonoBehaviour
     }
     private void Update()
     {
-
-        Vector2 newSize = sliderTransform.sizeDelta;
-        newSize.x++;
-        sliderTransform.sizeDelta = newSize;
+        Vector2 currentSize = sliderTransform.sizeDelta;
+        currentSize.x++;
+        sliderTransform.sizeDelta = currentSize;
 
         if (sliderTransform.sizeDelta.x >= sliderMaxWidth || Input.GetMouseButtonDown(0))
         {
-            newSize.x = sliderMinWidth;
-            sliderTransform.sizeDelta = newSize;
+            Debug.Log(currentSize.x);
+            currentSize.x=sliderMinWidth;
+            sliderTransform.sizeDelta = currentSize;
+
+            //if (currentSize.x > leftLimit && currentSize.x<rightLimit) 
+            //{
+            //    Debug.Log("Nice");
+            //    newSize.x = sliderMinWidth;
+            //    sliderTransform.sizeDelta = newSize;
+
+            //}
+            //else
+            //{
+            //    newSize.x = 0;
+            //    sliderTransform.sizeDelta = newSize;
+            //}
+
+
         }
 
     }
 
     private IEnumerator AlphaChange()
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(1);
 
         while (canvasGroup.alpha < 1)
         {
-            canvasGroup.alpha +=0.1f;
-
+            canvasGroup.alpha +=alphaChangeSpeed*Time.deltaTime;
+            yield return null;
         }
-        
+       
+        yield return new WaitForSeconds(2);
+
         if (sliderTransform != null)
         {
             Vector2 newSize = sliderTransform.sizeDelta;
             newSize.x = sliderMinWidth;
-            sliderTransform.sizeDelta = newSize;
+            yield return null;
+
+
+            //while (newSize.x<sliderMaxWidth)
+            //{
+            //    newSize.x++;
+            //    sliderTransform.sizeDelta = newSize;
+            //    yield return null;
+            //}
         }
       
     }
