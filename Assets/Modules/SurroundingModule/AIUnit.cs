ed7214f0487210efcf8d1eafa33d547da82c699a
ilia.lataria <lataria.dev@gmail.com>
2025-02-16 15:54:12 +0400

    states

--- Assets/Modules/SurroundingModule/AIUnit.cs
+++ Assets/Modules/SurroundingModule/AIUnit.cs
@@ -1,7 +1,9 @@
 using AloneTower;
+using AloneTower.SpawnSystem;
 using AloneTower.Towers;
 using UnityEngine;
 using UnityEngine.AI;
+using static UnityEngine.GraphicsBuffer;
 
 public class AIUnit : MonoBehaviour
 {
@@ -9,43 +11,126 @@ public class AIUnit : MonoBehaviour
     private Tower _tower;
 
     [SerializeField] private EnemyAttack _enemyAttack;
-
-    private bool _isArrived;
-    private bool _runState;
-    private bool _attackState;
+    public float RadiusAroundTarget = 1f;
+    private EnemyState _currentState;
+    public Vector3 _agentTargetPos;
+    private SpawnerModule _spawnerModule;
 
     private void Start()
     {
+        _spawnerModule = FindAnyObjectByType<SpawnerModule>();
         _tower = GameObject.FindObjectOfType<Tower>();
-        AIManager.Instance.Units.Add(this);
-        _runState = true;
     }
-    public void MoveTo(Vector3 Position)
+
+    private void Update()
     {
-
-        if (_runState)
+        switch (_currentState)
         {
-            _agent.SetDestination(Position);
-
-            if (_agent.pathPending)
-            {
-                Debug.Log($"Pending");
-                return;
-            }
-
-            if (_agent.remainingDistance <= _agent.stoppingDistance)
-            {
-                _isArrived = true;
-                _enemyAttack.Attack();
-
-                _runState = false;
-                _attackState = true;
-            }
+            case EnemyState.RunState:
+                RunStateUpdate();
+                break;
+            case EnemyState.AttackState:
+                AttackStateUpdate();
+                break;
+            default:
+                break;
         }
-        else if(_attackState)
+    }
+
+    public void AttackStateUpdates()
+    {
+        Debug.Log($"Attack state update");
+        _agent.enabled = false;
+        transform.parent.LookAt(_tower.transform);
+    }
+    public void RunStateEnter()
+    {
+        Vector3 targetPos = AIManager.Instance._aiTargetPositions.Dequeue();
+        Debug.Log($"Run state enter");
+        _agent.SetDestination(targetPos);
+    }
+
+    public void RunStateUpdate()
+    {
+        Debug.Log($"Run state update");
+
+        if (_agent.pathPending)
         {
-            _agent.enabled = false;
-            transform.parent.LookAt(_tower.transform);
+            Debug.Log($"Pending");
+            return;
         }
+
+        bool isArrived = _agent.remainingDistance <= .1f;
+
+        if (isArrived)
+        {
+            SetState(EnemyState.AttackState);
+            return;
+        }
+    }
+
+    public void RunStateExit()
+    {
+        Debug.Log($"Run state exit");
+    }
+
+    public void AttackStateEnter()
+    {
+        Debug.Log($"Attack state enter");
+        _agent.enabled = false;
+        transform.parent.LookAt(_tower.transform);
+    }
+
+    public void AttackStateUpdate()
+    {
+        _enemyAttack.Attack();
+        Debug.Log($"Attack state update");
+    }
+
+    public void AttackStateExit()
+    {
+        Debug.Log($"Attack state exit");
+    }
+
+    public void SetState(EnemyState targetState)
+    {
+        if (_currentState == targetState)
+        {
+            Debug.Log($"Already in the state");
+            return;
+        }
+
+        switch (_currentState)
+        {
+            case EnemyState.RunState:
+                RunStateExit();
+                break;
+            case EnemyState.AttackState:
+                AttackStateExit();
+                break;
+            default:
+                break;
+        }
+
+        _currentState = targetState;
+
+        switch (_currentState)
+        {
+            case EnemyState.RunState:
+                RunStateEnter();
+                break;
+            case EnemyState.AttackState:
+                AttackStateEnter();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public enum EnemyState
+    {
+        IdleState,
+        RunState,
+        AttackState
     }
 }
